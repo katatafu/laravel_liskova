12c06b5a6eba71ccc47f7639a3b139e0e99e7d45
Alena Liskova <liskova.aja@gmail.com>
2024-09-17 08:31:30 +0200

    Hello_world!

---
+++ example-app¨/app/Http/Controllers/Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+abstract class Controller
+{
+    // 
+}
+
+// // nachází se tam všechny související soubory pro HTTP requesty, včetně kontrolerů, middleware a formulářů
---
+++ example-app¨/app/Providers/AppServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
+
+// umožňuje registrovat a konfigurovat služby a komponenty aplikace, čímž zajišťuje jejich správnou inicializaci a dostupnost
